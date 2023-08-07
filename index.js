028b786ebb53a18480e5f414c36a808c86bc6b38
phkla1 <phkla1@localhost>
2023-08-07 06:41:32 +0100

    apiRouter on /api/v3 rxjs installation to solve pm2 (how come it was working before?)

--- index.js
+++ index.js
@@ -32,19 +32,23 @@ dbConnection.sync().then(() => {
 	console.error('Unable to create database: ', error);
 });
 
+const apiPrefix = '/api/v3';
+const apiRouter = express.Router();
+app.use(apiPrefix, apiRouter);
+
 const accountsRoutes = require('./routes/accountsRoutes');
-app.use('/accounts', accountsRoutes);
+apiRouter.use('/accounts', accountsRoutes);
 
 const sellersRoutes = require('./routes/sellerRoutes');
-app.use('/sellers',  sellersRoutes);
+apiRouter.use('/sellers',  sellersRoutes);
 
 const productsRoutes = require('./routes/productsRoutes');
-app.use('/products',  productsRoutes);
+apiRouter.use('/products',  productsRoutes);
 
 const ordersRoutes = require('./routes/ordersRoutes');
-app.use('/orders',  ordersRoutes);
+apiRouter.use('/orders',  ordersRoutes);
 
 const url = `${process.env.PORT || 3000}`;
 app.listen(url, () => {
-	console.log('Starting UniteV3 on port 3000');
+	console.log(`Starting UniteV3 on port ${process.env.PORT || 3000}`);
 });
